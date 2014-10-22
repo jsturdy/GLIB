faf8baff6b1be3c07ac8382387cc0fc3c91c314e
Thomas Lenzi <tlenzi@ulb.ac.be>
2014-10-22 15:37:43 +0200

    Added software

--- software/loop.py
+++ software/loop.py
@@ -76,39 +76,6 @@ def testWrite(vfat2, register, value):
         return False
         pass
 
-def randomRW():
-    val8 = random.randint(0, 255)
-    val9 = random.randint(0, 255)
-    val10 = random.randint(0, 255)
-    val11 = random.randint(0, 255)
-    val12 = random.randint(0, 255)
-    val13 = random.randint(0, 255)
-
-    res8 = 0x3080000 + val8
-    res9 = 0x3090100 + val9
-    res10 = 0x30a9500 + val10
-    res11 = 0x30b0000 + val11
-    res12 = 0x30c0100 + val12
-    res13 = 0x30d9500 + val13
-
-    print "Random: ", hex(val8), hex(val9), hex(val10), hex(val11), hex(val12), hex(val13)
-    wr8 = testWrite(8, "ctrl0", val8)
-    wr9 = testWrite(9, "ctrl1", val9)
-    wr10 = testWrite(10, "ctrl2", val10)
-    if wr9:
-        testRead(9, "ctrl1", res9)
-    if wr8:
-        testRead(8, "ctrl0", res8)
-    wr11 = testWrite(11, "ctrl0", val11)
-    if wr10:
-        testRead(10, "ctrl2", res10)
-    wr12 = testWrite(12, "ctrl1", val12)
-    wr13 = testWrite(13, "ctrl2", val13)
-    if wr13:
-        testRead(13, "ctrl2", res13)
-    if wr12:
-        testRead(12, "ctrl1", res12)
-
 def signal_handler(signal, frame):
     global nOK, nBadHeader, nErrorOnRead, nTimedOut, nMismatch, nOthers, timeStart
 
@@ -149,29 +116,8 @@ if __name__ == "__main__":
 
     while True:
 
-        if (time.time() - timeStart > 60):
-            break
-
-        testRead(8, "chipid0", 0x30808cf)
-        testRead(9, "chipid0", 0x3090854)
-        testRead(10, "chipid0", 0x30a08e4)
-        testRead(11, "chipid0", 0x30b086c)
-        testRead(12, "chipid0", 0x30c0868)
-        testRead(13, "chipid0", 0x30d0868)
-
-
-
-        # randomRW()
-
-        # testRead(8,  "chipid0", 0x30808e4)
-        # testRead(8,  "chipid1", 0x30809f0)
-        # testRead(9,  "chipid0", 0x30908cf)
-        # testRead(9,  "chipid1", 0x30909fe)
-        # testRead(10, "chipid0", 0x30a0854)
-        # testRead(10, "chipid1", 0x30a09f0)
-
-        # print "----------------"
-        # raw_input()
+        testRead(12, "chipid0", 0x00)
+        time.sleep(0.1)
 
     signal_handler(0, 0)
 
